781e74b729e9a84c3d9783ff3c6d1b88fc2d7223
Syed Farhan <syedfarhan596@gmail.com>
2023-10-01 01:34:40 +0530

    fix allowed host

--- audiomix/settings.py
+++ audiomix/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['103.224.159.71/32']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
